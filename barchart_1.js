704153482d138c443e81efc0bc1f50e5d8c1ec26
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-10-27 00:08:59 +0200

    Add files via upload

---
+++ barchart_1.js
@@ -0,0 +1,46 @@
+// set the dimensions and margins of the graph
+const margin = {top: 40, right: 40, bottom: 50, left: 120}, width = 700 - margin.left - margin.right, height = 500 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#barchart_1")
+              .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Parse the Data
+d3.csv("http://localhost:8888/barchart_1.csv").then( function(data) {
+
+    // Add X axis
+    const x = d3.scaleLinear()
+                .domain([0, 250000])
+                .range([ 0, width]);
+
+    svg.append("g")
+       .attr("transform", `translate(0, ${height})`)
+       .call(d3.axisBottom(x))
+       .selectAll("text")
+       .attr("transform", "translate(-10,0)rotate(-45)")
+       .style("text-anchor", "end");
+
+    // Y axis
+    const y = d3.scaleBand()
+                .range([ height, 0 ])
+                .domain(data.map(d => d.name))
+                .padding(.1);
+
+    svg.append("g")
+       .call(d3.axisLeft(y))
+
+    // Bars
+    svg.selectAll("myRect")
+       .data(data)
+       .join("rect")
+       .attr("x", x(0) )
+       .attr("y", d => y(d.name))
+       .attr("width", d => x(d.count))
+       .attr("height", y.bandwidth())
+       .attr("fill", "steelblue")
+
+})
