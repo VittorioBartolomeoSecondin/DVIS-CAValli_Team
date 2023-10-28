ff109bca9ca5cf8960762d94cbe2c9dbfccd9eab
Ale8599 <148397539+Ale8599@users.noreply.github.com>
2023-10-28 14:34:52 +0200

    Update barchart_1.js

--- barchart_1.js
+++ barchart_1.js
@@ -39,6 +39,14 @@ d3.csv("barchart_1.csv").then( function(data) {
     svg.append("g")
        .call(d3.axisLeft(y));
 
+    // Animation
+    svg.selectAll("rect")
+        .transition()
+        .duration(800)
+        .attr("y", d => y(d.Value))
+        .attr("height", d => height - y(d.Value))
+        .delay((d,i) => {console.log(i); return i*100})
+
     // Bars
     svg.selectAll("myRect")
        .data(data)
