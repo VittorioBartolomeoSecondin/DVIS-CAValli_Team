e55eced7f2812c4b8416110fc1c9ea19559ddc47
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-10-28 13:46:43 +0200

    Update barchart_1.js

--- barchart_1.js
+++ barchart_1.js
@@ -1,5 +1,6 @@
 // set the dimensions and margins of the graph
 const margin = {top: 40, right: 40, bottom: 50, left: 120}, width = 700 - margin.left - margin.right, height = 500 - margin.top - margin.bottom;
+
 // append the svg object to the body of the page
 const svg = d3.select("#barchart_1")
               .append("svg")
@@ -7,13 +8,16 @@ const svg = d3.select("#barchart_1")
               .attr("height", height + margin.top + margin.bottom)
               .append("g")
               .attr("transform", `translate(${margin.left}, ${margin.top})`);
-///////////////////////// Create the tooltip element
+
+// Create the tooltip element
 const tooltip = d3.select("#barchart_1")
-                  .append("div")
                   .attr("class", "tooltip")
                   .style("opacity", 0);
+                  //.append("div")
+
 // Parse the Data
 d3.csv("barchart_1.csv").then( function(data) {
+    
     // Add X axis
     const x = d3.scaleLinear()
                 .domain([0, 250000])
@@ -24,6 +28,7 @@ d3.csv("barchart_1.csv").then( function(data) {
        .selectAll("text")
        .attr("transform", "translate(-10,0)rotate(-45)")
        .style("text-anchor", "end");
+    
     // Y axis
     const y = d3.scaleBand()
                 .range([ height, 0 ])
@@ -36,27 +41,33 @@ d3.csv("barchart_1.csv").then( function(data) {
     // Bars
     svg.selectAll("myRect")
        .data(data)
-       .join("rect")
+       .enter()
+       .append("rect")
        .attr("x", x(0) )
        .attr("y", d => y(d.scientific_name))
        .attr("width", d => x(d.count))
        .attr("height", y.bandwidth())
        .attr("fill", "steelblue")
        .on("mouseover", function (event, d) {
+         
          // Show the tooltip
          tooltip.transition()
                 .duration(200)
                 .style("opacity", 1);
      
          // Customize the tooltip content
-         tooltip.html(`Common name: ${d.common_name}<br>Average height: ${d.avg_height}`)
+         tooltip.html(`Common name: ${d.common_name}<br>Average height: ${d.avg_height} m`)
                 .style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 28) + "px");
+         
        })
+      
        .on("mouseout", function (d) {
+         
          // Hide the tooltip
          tooltip.transition()
            .duration(500)
            .style("opacity", 0);
+         
        });
 })
