686279a2595598637fa1347add4c4a6b1a08b04b
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-10-30 19:40:34 +0100

    Update stacked_1.js

--- stacked_1.js
+++ stacked_1.js
@@ -1,78 +1,3 @@
-/*
-// set the dimensions and margins of the graph
-//var margin = {top: 10, right: 30, bottom: 20, left: 50},
-   // width = 460 - margin.left - margin.right,
-    //height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#stacked_1")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// Parse the Data
-d3.csv("section1_1/stacked_1.csv", function(data) {
-
-   console.log(data);
-
-  // List of subgroups = header of the csv files = scientific name of the tree (here)
-  var subgroups = data.columns.slice();
-
-  // List of groups = cities (here) = value of the first column called city -> I show them on the X axis
-  var groups = d3.map(data, function(d){return(d.city)}).keys();
-
-  // Add X axis
-  var x = d3.scaleBand()
-      .domain(groups)
-      .range([0, width])
-      .padding([0.2]);
-   
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSizeOuter(0));
-
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 60])
-    .range([ height, 0 ]);
-   
-  svg.append("g")
-    .call(d3.axisLeft(y));
-
-  // color palette = one color per subgroup
-  var color = d3.scaleOrdinal()
-    .domain(subgroups)
-    .range(['#e41a1c','#377eb8','#4daf4a', '#fcff33', '#c733ff']);
-
-  //stack the data? --> stack per subgroup
-  var stackedData = d3.stack()
-    .keys(subgroups)
-    (data)
-
-  // Show the bars
-  svg.append("g")
-    .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
-    .data(stackedData)
-    .enter().append("g")
-      .attr("fill", function(d) { return color(d.key); })
-      .selectAll("rect")
-      // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data(function(d) { return d; })
-      .enter().append("rect")
-        .attr("x", function(d) { return x(d.data.city); })
-        .attr("y", function(d) { return y(d[1]); })
-        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-        .attr("width",x.bandwidth());
-})
-*/
-// set the dimensions and margins of the graph
-//const margin = {top: 10, right: 30, bottom: 20, left: 50},
-    //width = 460 - margin.left - margin.right,
-    //height = 400 - margin.top - margin.bottom;
-
 // append the svg object to the body of the page
 const svg = d3.select("#stacked_1")
   .append("svg")
@@ -90,8 +15,8 @@ d3.csv("section1_1/stacked_1.csv").then( function(data) {
   // List of groups = species here = value of the first column called group -> I show them on the X axis
   const groups = data.map(d => (d.city))
 
-  // Add X axis
-  const x = d3.scaleBand()
+  // Add Y axis
+  const y = d3.scaleBand()
       .domain(groups)
       .range([0, width])
       .padding([0.2])
@@ -99,8 +24,8 @@ d3.csv("section1_1/stacked_1.csv").then( function(data) {
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x).tickSizeOuter(0));
 
-  // Add Y axis
-  const y = d3.scaleLinear()
+  // Add X axis
+  const x = d3.scaleLinear()
     .domain([0, 60])
     .range([ height, 0 ]);
   svg.append("g")
@@ -127,8 +52,8 @@ d3.csv("section1_1/stacked_1.csv").then( function(data) {
       // enter a second time = loop subgroup per subgroup to add all rectangles
       .data(d => d)
       .join("rect")
-        .attr("x", d => x(d.data.city))
-        .attr("y", d => y(d[1]))
-        .attr("height", d => y(d[0]) - y(d[1]))
-        .attr("width",x.bandwidth())
+        .attr("y", d => y(d.data.city))
+        .attr("x", d => x(d[1]))
+        .attr("height", y.bandwidth())
+        .attr("width", d => x(d[0]) - x(d[1]))
 })
