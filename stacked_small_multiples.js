48011366d52ea1cfb149c993791d1046bb91d21b
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-02 21:09:35 +0100

    Update stacked_small_multiples.js

--- stacked_small_multiples.js
+++ stacked_small_multiples.js
@@ -43,118 +43,117 @@ function updateStackedSMChart(selectedValue) {
         // All data has been loaded and max_values is populated
         max = Math.max(...max_values);
         console.log(max);
+          
         // Now you can continue with your chart creation or other actions
+        for (let i = 0; i < datasets.length; i++) {
+
+              // Parse the Data
+              d3.csv(datasets[i]).then( function(data) {
+                 var filteredData = data;
+                 if (selectedValue == "all")
+                    filteredData = data.slice(0);
+                 else
+                    filteredData = data.slice(0, selectedValue);
+                  // Append the svg object to the body of the page
+                  const svg = d3.select("#" + datasets[i].substring(11, 26))
+                                .append("svg")
+                                .attr("id", datasets[i].substring(11, 26) + "_svg")
+                                  .attr("width", width + margin.left + margin.right)
+                                  .attr("height", height + margin.top + margin.bottom)
+                                .append("g")
+                                  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+            
+                  // Create the tooltip element
+                  const tooltip = d3.select("#" + datasets[i].substring(11, 26))
+                                    .append("section")
+                                    .attr("id", datasets[i].substring(11, 26) + "_tooltip")
+                                    .style("opacity", 0)
+                                    .style("background-color", "lightgray")
+                                    .style("border", "2px solid black")
+                                      .attr("class", "tooltip");
+        
+                 
+                  // Add X axis
+                  const x = d3.scaleLinear()
+                              .domain([0, max + max/10])
+                              .range([0, width]);
+                
+                  svg.append("g")
+                       .attr("transform", `translate(0, ${height})`)
+                     .call(d3.axisBottom(x))
+                     .selectAll("text")
+                       .attr("transform", "translate(-10,0)rotate(-45)")
+                     .style("text-anchor", "end");
+                  
+                  // Add Y axis
+                  const y = d3.scaleBand()
+                              .range([0, height])
+                              .domain(filteredData.map(d => d.city))
+                              .padding(.1);
+              
+                  svg.append("g")
+                     .call(d3.axisLeft(y));
+              
+                  // Show the bars
+                  svg.selectAll("myRect")
+                     .data(filteredData)
+                     .enter()
+                     .append("rect")
+                       .attr("x", x(0))
+                       .attr("y", d => y(d.city))
+                       //.attr("width", d => x(d.count))
+                       .attr("width", 0)
+                       .attr("height", y.bandwidth())
+                       .attr("fill", colours[i])
+                     .on("mouseover", function (event, d) {
+            
+                     // Change color when hovering
+                     d3.select(this).style("fill", "lightgreen");
+            
+                     // Show the tooltip
+                     tooltip.transition()
+                            .duration(200)
+                            .style("opacity", 1)
+                            .style("background-color", "lightgray")
+                            .style("border", "2px solid black");
+                     
+                     // Customize the tooltip content
+                     tooltip.html(`Count: ${d.count}`)
+                            .style("left", (event.pageX + 40) + "px")
+                            .style("top", (event.pageY - 40) + "px");
+            
+                     })
+                     .on("mousemove", function (event, d) {
+            
+                     // Move the tooltip with the mouse pointer
+                     tooltip.style("left", (event.pageX + 10) + "px")
+                            .style("top", (event.pageY + 10) + "px");
+            
+                     })
+                     .on("mouseout", function (d) {
+            
+                     // Returning to original color when not hovering
+                     d3.select(this).style("fill", colours[i]);
+            
+                     // Hide the tooltip
+                     tooltip.transition()
+                            .duration(500)
+                            .style("opacity", 0);           
+                     });  
+            
+                  // Animation
+                  svg.selectAll("rect")
+                      .transition()
+                      .duration(1000)
+                        .attr("x", x(0))
+                        .attr("width", d => x(d.count))
+                      .delay((d, i) => i * 100);
+              })
+        }
       })
       .catch((error) => {
         console.error(error);
       });
-
-    
-    for (let i = 0; i < datasets.length; i++) {
-
-      // Parse the Data
-      d3.csv(datasets[i]).then( function(data) {
-        var filteredData = data;
-        if (selectedValue == "all")
-            filteredData = data.slice(0);
-        else
-            filteredData = data.slice(0, selectedValue);
-        // Append the svg object to the body of the page
-          const svg = d3.select("#" + datasets[i].substring(11, 26))
-                        .append("svg")
-                        .attr("id", datasets[i].substring(11, 26) + "_svg")
-                          .attr("width", width + margin.left + margin.right)
-                          .attr("height", height + margin.top + margin.bottom)
-                        .append("g")
-                          .attr("transform", `translate(${margin.left}, ${margin.top})`);
-    
-          // Create the tooltip element
-          const tooltip = d3.select("#" + datasets[i].substring(11, 26))
-                            .append("section")
-                            .attr("id", datasets[i].substring(11, 26) + "_tooltip")
-                            .style("opacity", 0)
-                            .style("background-color", "lightgray")
-                            .style("border", "2px solid black")
-                              .attr("class", "tooltip");
-
-         
-          // Add X axis
-          const x = d3.scaleLinear()
-                      .domain([0, max + max/10])
-                      .range([0, width]);
-        
-          svg.append("g")
-               .attr("transform", `translate(0, ${height})`)
-             .call(d3.axisBottom(x))
-             .selectAll("text")
-               .attr("transform", "translate(-10,0)rotate(-45)")
-             .style("text-anchor", "end");
-          
-          // Add Y axis
-          const y = d3.scaleBand()
-                      .range([0, height])
-                      .domain(filteredData.map(d => d.city))
-                      .padding(.1);
-      
-          svg.append("g")
-             .call(d3.axisLeft(y));
-      
-          // Show the bars
-          svg.selectAll("myRect")
-             .data(filteredData)
-             .enter()
-             .append("rect")
-               .attr("x", x(0))
-               .attr("y", d => y(d.city))
-               //.attr("width", d => x(d.count))
-               .attr("width", 0)
-               .attr("height", y.bandwidth())
-               .attr("fill", colours[i])
-             .on("mouseover", function (event, d) {
-    
-             // Change color when hovering
-             d3.select(this).style("fill", "lightgreen");
-    
-             // Show the tooltip
-             tooltip.transition()
-                    .duration(200)
-                    .style("opacity", 1)
-                    .style("background-color", "lightgray")
-                    .style("border", "2px solid black");
-             
-             // Customize the tooltip content
-             tooltip.html(`Count: ${d.count}`)
-                    .style("left", (event.pageX + 40) + "px")
-                    .style("top", (event.pageY - 40) + "px");
-    
-             })
-             .on("mousemove", function (event, d) {
-    
-             // Move the tooltip with the mouse pointer
-             tooltip.style("left", (event.pageX + 10) + "px")
-                    .style("top", (event.pageY + 10) + "px");
-    
-             })
-             .on("mouseout", function (d) {
-    
-             // Returning to original color when not hovering
-             d3.select(this).style("fill", colours[i]);
-    
-             // Hide the tooltip
-             tooltip.transition()
-                    .duration(500)
-                    .style("opacity", 0);           
-             });  
-    
-          // Animation
-          svg.selectAll("rect")
-              .transition()
-              .duration(1000)
-                .attr("x", x(0))
-                .attr("width", d => x(d.count))
-              .delay((d, i) => i * 100);
-      })
-    }
 }
 updateStackedSMChart("all"); // Load the chart with all cities initially
 document.getElementById("city-dropdown").addEventListener("change", function () {
