f6d366efdd8fd436f863148b6cec710ea553eaf3
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-03 11:03:12 +0100

    Update barchart_1.js

--- barchart_1.js
+++ barchart_1.js
@@ -1,6 +1,6 @@
 // Set the dimensions and margins of the graph
 const margin = {top: 40, right: 40, bottom: 50, left: 140}, width = 1000 - margin.left - margin.right, height = 600 - margin.top - margin.bottom;
-const margin2 = {top: 40, right: 40, bottom: 50, left: 140}, width = 450 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
+const margin2 = {top: 40, right: 40, bottom: 50, left: 140}, width2 = 450 - margin.left - margin.right, height2 = 400 - margin.top - margin.bottom;
 
 // Parse the Data
 function updateBarChart(selectedDataset) {
