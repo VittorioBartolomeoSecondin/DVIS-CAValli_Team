d951f60ac85b2d3e15068acf2d0a52e702e47b94
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-03 17:40:17 +0100

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -1,47 +1,61 @@
 // append the svg object to the body of the page
-    const svg = d3.select("#heatmap")
-                  .append("svg")
-                  .attr("id", "heatmap_svg")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                  .attr("transform", `translate(${margin.left},${margin.top})`);
+const svg = d3.select("#heatmap")
+.append("svg")
+.attr("id", "heatmap_svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
+d3.csv("section1_1/heatmap.csv").then(function(data) {
 
-    // Labels of row and columns
-    const myGroups = ["Los Angeles", "San Jose", "San Diego", "Rancho Cucamonga", "Stockton"]
-    const myVars = ["Lagerstroemia indica", "Platanus acerifolia", "Liquidambar styraciflua", "Washingtonia robusta", "Magnolia grandiflora"]
+  // Labels of row and columns -> unique identifier of the column called 'city' and 'species'
+  const myGroups = Array.from(new Set(data.map(d => d.city)))
+  const myVars = Array.from(new Set(data.map(d => d.species)))
 
-    // Build X scales and axis:
-    const x = d3.scaleBand()
-                .range([ 0, width ])
-                .domain(myGroups)
-                .padding(0.01);
-                svg.append("g")
-                .attr("transform", `translate(0, ${height})`)
-                .call(d3.axisBottom(x))
+  // Build X scales and axis:
+  const x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(myGroups)
+    .padding(0.05);
+  svg.append("g")
+    .style("font-size", 15)
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x).tickSize(0))
+    .select(".domain").remove()
 
-    // Build X scales and axis:
-    const y = d3.scaleBand()
-                .range([ height, 0 ])
-                .domain(myVars)
-                .padding(0.01);
-                svg.append("g")
-                .call(d3.axisLeft(y));
+  // Build Y scales and axis:
+  const y = d3.scaleBand()
+    .range([ height, 0 ])
+    .domain(myVars)
+    .padding(0.05);
+  svg.append("g")
+    .style("font-size", 15)
+    .call(d3.axisLeft(y).tickSize(0))
+    .select(".domain").remove()
 
-    // Build color scale
-    const myColor = d3.scaleLinear()
-                      .range(["#d5e9c5", "#356d10"])
-                      .domain([500,40000])
+  // Build color scale
+  const myColor = d3.scaleSequential()
+    .interpolator(d3.interpolateInferno)
+    .domain([0,40000])
 
-    svg.selectAll()
-       .data(data, function(d) {return d.group+':'+d.variable;})
-       .join("rect")
-       .attr("x", function(d) { return x(d.group) })
-       .attr("y", function(d) { return y(d.variable) })
-       .attr("width", x.bandwidth() )
-       .attr("height", y.bandwidth() )
-       .style("fill", function(d) { return myColor(d.value)} )
+  
+
+
+
+  // add the squares
+  svg.selectAll()
+    .data(data, function(d) {return d.city+':'+d.species;})
+    .join("rect")
+      .attr("x", function(d) { return x(d.city) })
+      .attr("y", function(d) { return y(d.species) })
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .attr("width", x.bandwidth() )
+      .attr("height", y.bandwidth() )
+      .style("fill", function(d) { return myColor(d.count)} )
+      .style("stroke-width", 4)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
 })
