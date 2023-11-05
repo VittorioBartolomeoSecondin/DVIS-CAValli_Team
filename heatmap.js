814ff1b2d5b2926f001209115f31f75eaacf4212
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-05 17:24:20 +0100

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -61,4 +61,58 @@ d3.csv("section1_1/heatmap.csv").then(function(data) {
       .style("stroke-width", 1)
       .style("stroke", "black")
       .style("opacity", 0.8)
+
+  var colorscale = colours.reverse();
+  var color = d3.scaleLinear()
+    .domain([d3.min(data, function(d) { return +d.count}), d3.max(data, function(d) { return +d.count})])
+    .range(colorscale);
+  
+  var format = d3.format(".1f")
+  
+  drawColorScale();
+  
+  function drawColorScale() {
+    var pallete = svg.append('g')
+      .attr('id', 'pallete');
+  
+    var swatch = pallete.selectAll('rect').data(colorscale);
+    swatch.enter().append('rect')
+      .attr('fill', function(d) {
+        return d;
+      })
+      .attr('x', function(d, i) {
+        return i * 50;
+      })
+      .attr('y', 50)
+      .attr('width', 50)
+      .attr('height', 10);
+  
+    var texts = pallete.selectAll("foo")
+      .data(color.range())
+      .enter()
+      .append("text")
+      .attr("font-size", "10px")
+      .attr("text-anchor", "middle")
+      .attr("y", 80)
+      .attr('x', function(d, i) {
+        return i * 50 + 25;
+      })
+      .text(function(d) {
+        return format(color.invertExtent(d)[0])
+      })
+      .append("tspan")
+      .attr("dy", "1.3em")
+      .attr('x', function(d, i) {
+        return i * 50 + 25;
+      })
+      .text("to")
+      .append("tspan")
+      .attr("dy", "1.3em")
+      .attr('x', function(d, i) {
+        return i * 50 + 25;
+      })
+      .text(function(d) {
+        return format(color.invertExtent(d)[1])
+      })
+  }
 })
