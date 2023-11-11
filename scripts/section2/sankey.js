46e808376e66e0756430031830d7565b4eeb6e2b
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-11-11 16:41:21 +0100

    Update sankey.js

--- scripts/section2/sankey.js
+++ scripts/section2/sankey.js
@@ -10,11 +10,11 @@ var formatNumber = d3.format(",.0f"), // zero decimal places
   
 // append the svg object to the body of the page
 var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", 
+	          "translate(" + margin.left + "," + margin.top + ")");
 
 // Set the sankey diagram properties
 var sankey = d3.sankey()
@@ -39,7 +39,7 @@ d3.csv("data/section2/sankey.csv").then(function(data) {
    });
 
   // return only the distinct / unique nodes
- sankeydata.nodes = Array.from(
+  sankeydata.nodes = Array.from(
     d3.group(sankeydata.nodes, d => d.name),
 	([value]) => (value)
   );
@@ -60,7 +60,7 @@ d3.csv("data/section2/sankey.csv").then(function(data) {
 
   graph = sankey(sankeydata);
 
-// add in the links
+  // add in the links
   var link = svg.append("g").selectAll(".link")
       .data(graph.links)
     .enter().append("path")
@@ -68,49 +68,42 @@ d3.csv("data/section2/sankey.csv").then(function(data) {
       .attr("d", d3.sankeyLinkHorizontal())
       .attr("stroke-width", function(d) { return d.width; });  
 
-// add the link titles
+  // add the link titles
   link.append("title")
         .text(function(d) {
     		    return d.source.name + " → " + 
                 d.target.name + "\n" + format(d.value); });
 
-// add in the nodes
+  // add in the nodes
   var node = svg.append("g").selectAll(".node")
       .data(graph.nodes)
     .enter().append("g")
-      .attr("class", "node");
 
-// add in the nodes
-var node = svg.append("g").selectAll(".node")
-    .data(graph.nodes)
-  .enter().append("g")
-    .attr("class", "node");
+  // add the rectangles for the nodes
+  node.append("rect")
+      .attr("x", function(d) { return d.x0; })
+      .attr("y", function(d) { return d.y0; })
+      .attr("height", function(d) { return d.y1 - d.y0; })
+      .attr("width", sankey.nodeWidth())
+      .style("fill", function(d) { 
+	  return d.color = color(d.name.replace(/ .*/, "")); })
+      .style("stroke", function(d) { 
+	  return d3.rgb(d.color).darker(2); });
 
-// add the rectangles for the nodes
-node.append("rect")
-    .attr("x", function(d) { return d.x0; })
-    .attr("y", function(d) { return d.y0; })
-    .attr("height", function(d) { return d.y1 - d.y0; })
-    .attr("width", sankey.nodeWidth())
-    .style("fill", function(d) { 
-        return d.color = color(d.name.replace(/ .*/, "")); })
-    .style("stroke", function(d) { 
-        return d3.rgb(d.color).darker(2); });
+  // add the title for the nodes
+  node.append("title")
+      .text(function(d) { 
+          return d.name + "\n" + format(d.value); });
 
-// add the title for the nodes
-node.append("title")
-    .text(function(d) { 
-        return d.name + "\n" + format(d.value); });
-
-// add in the text for the nodes
-node.append("text")
-    .attr("x", function(d) { return d.x0 - 6; })
-    .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
-    .attr("dy", "0.35em")
-    .attr("text-anchor", "end")
-    .text(function(d) { return d.name; })
-  .filter(function(d) { return d.x0 < width / 2; })
-    .attr("x", function(d) { return d.x1 + 6; })
-    .attr("text-anchor", "start");
+  // add in the text for the nodes
+  node.append("text")
+      .attr("x", function(d) { return d.x0 - 6; })
+      .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
+      .attr("dy", "0.35em")
+      .attr("text-anchor", "end")
+      .text(function(d) { return d.name; })
+    .filter(function(d) { return d.x0 < width / 2; })
+      .attr("x", function(d) { return d.x1 + 6; })
+      .attr("text-anchor", "start");
 
 });
