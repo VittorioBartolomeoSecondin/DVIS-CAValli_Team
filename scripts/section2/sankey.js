537e928eea12e2c8732a77bcee5d46d559c3f3b3
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-11-11 16:37:27 +0100

    Update sankey.js

--- scripts/section2/sankey.js
+++ scripts/section2/sankey.js
@@ -80,29 +80,37 @@ d3.csv("data/section2/sankey.csv").then(function(data) {
     .enter().append("g")
       .attr("class", "node");
 
-// add the rectangles for the nodes
-  node.append("rect")
-      .attr("x", function(d) { return d.x0; })
-      .attr("y", function(d) { return d.y0; })
-      .attr("height", function(d) { return d.y1 - d.y0; })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function(d) { 
-		      return d.color = color(d.name.replace(/ .*/, "")); })
-      .style("stroke", function(d) { 
-		  return d3.rgb(d.color).darker(2); })
-	  .append("title")
-      .text(function(d) { 
-		  return d.name + "\n" + format(d.value); });
+// add in the nodes
+var node = svg.append("g").selectAll(".node")
+    .data(graph.nodes)
+  .enter().append("g")
+    .attr("class", "node");
 
-// add in the title for the nodes
-  node.append("text")
-      .attr("x", function(d) { return d.x0 - 6; })
-      .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
-      .attr("dy", "0.35em")
-      .attr("text-anchor", "end")
-      .text(function(d) { return d.name; })
-    .filter(function(d) { return d.x0 < width / 2; })
-      .attr("x", function(d) { return d.x1 + 6; })
-      .attr("text-anchor", "start");
+// add the rectangles for the nodes
+node.append("rect")
+    .attr("x", function(d) { return d.x0; })
+    .attr("y", function(d) { return d.y0; })
+    .attr("height", function(d) { return d.y1 - d.y0; })
+    .attr("width", sankey.nodeWidth())
+    .style("fill", function(d) { 
+        return d.color = color(d.name.replace(/ .*/, "")); })
+    .style("stroke", function(d) { 
+        return d3.rgb(d.color).darker(2); });
+
+// add the title for the nodes
+node.append("title")
+    .text(function(d) { 
+        return d.name + "\n" + format(d.value); });
+
+// add in the text for the nodes
+node.append("text")
+    .attr("x", function(d) { return d.x0 - 6; })
+    .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
+    .attr("dy", "0.35em")
+    .attr("text-anchor", "end")
+    .text(function(d) { return d.name; })
+  .filter(function(d) { return d.x0 < width / 2; })
+    .attr("x", function(d) { return d.x1 + 6; })
+    .attr("text-anchor", "start");
 
 });
