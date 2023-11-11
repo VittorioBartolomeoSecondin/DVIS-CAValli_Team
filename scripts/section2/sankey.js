2d0b2be4ddd5c464d2de073709a6ed559d2f1801
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-11 17:05:33 +0100

    Update sankey.js

--- scripts/section2/sankey.js
+++ scripts/section2/sankey.js
@@ -66,8 +66,8 @@ d3.csv("data/section2/sankey.csv").then(function(data) {
     .enter().append("path")
       .attr("class", "link")
       .attr("d", d3.sankeyLinkHorizontal())
-      /*.attr("stroke-width", function(d) { return d.width; });  */
-      .attr("stroke-width", 1);  
+      .attr("stroke-width", function(d) { print(d.width); return d.width; });
+      //.attr("stroke-width", 1);  
 
   // add the link titles
   link.append("title")
