57955243e2e3a9519edc7ecff604149f8e41a103
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-12 16:45:38 +0100

    Update sankey.js

--- scripts/section2/sankey.js
+++ scripts/section2/sankey.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 40, bottom: 70, left: 60},
+var margin = {top: 20, right: 40, bottom: 70, left: 40},
     width = 1500 - margin.left - margin.right,
-    height = 1400 - margin.top - margin.bottom;  
+    height = 1200 - margin.top - margin.bottom;  
 
 // format variables
 var formatNumber = d3.format(",.0f"), // zero decimal places
