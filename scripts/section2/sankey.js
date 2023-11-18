e87f9eb8534290145e2fab0e357503c900b3b553
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-18 13:11:05 +0100

    Update sankey.js

--- scripts/section2/sankey.js
+++ scripts/section2/sankey.js
@@ -114,7 +114,30 @@ d3.csv("data/section2/sankey_alternative.csv").then(function(data) {
       .attr("x", function(d) { return d.x1 + 6; })
       .attr("text-anchor", "start");
 
-  var showing_connections = false;
+   var showing_connections = false;
+
+// Add hover effects to nodes
+node.on("click", function(event, d) {
+    d3.select(this).attr("font-weight", "bold");
+
+    // Toggle the state when a node is clicked
+    showing_connections = !showing_connections;
+
+    // Update the link styles based on the state
+    link.each(function(linkData) {
+        if (linkData.target.name === d.name) {
+            var linkElement = d3.select(document.getElementById(linkData.source.name + "->" + d.name));
+            if (showing_connections) {
+                linkElement.style("stroke-opacity", 0.5)
+                           .attr("stroke-width", function(d) { return Math.max(4, d.width); });
+            } else {
+                linkElement.style("stroke-opacity", 0.2)
+                           .attr("stroke-width", function(d) { return d.width; });
+            }
+        }
+    });
+});
+  /*var showing_connections = false;
 	
   // Add hover effects to nodes
   node.on("click", function (event, d) {
@@ -136,7 +159,7 @@ d3.csv("data/section2/sankey_alternative.csv").then(function(data) {
 	       }
 	   }
        });
-   })
+   })*/
      /* .on("mouseout", function (event, d) {
          d3.select(this).attr("font-weight", "normal");
 
