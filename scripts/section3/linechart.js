c886934279aad0f88f0d2197360007a37fd38167
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2023-11-21 14:54:56 +0100

    Update linechart.js

--- scripts/section3/linechart.js
+++ scripts/section3/linechart.js
@@ -27,6 +27,10 @@ function updateLineChart(selectedDataset_1,selectedDataset_2,selectedDataset_3) 
         var yearDataAvg = dataAvg.filter(function (d) { return d.year === 1895; });
         var yearDataMax = dataMax.filter(function (d) { return d.year === 1895; });
         var yearDataMin = dataMin.filter(function (d) { return d.year === 1895; });
+
+        console.log(yearDataAvg);
+        console.log(yearDataMax);
+        console.log(yearDataMin);
     
         var allMonths = Object.keys(dataAvg[0]).slice(2);
         var months = allMonths.slice(0, allMonths.length / 2);
