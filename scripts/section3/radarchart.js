b19cb36749600d71b1d0c99b54a9f1b547a935f0
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-11-25 17:01:08 +0100

    Update radarchart.js

--- scripts/section3/radarchart.js
+++ scripts/section3/radarchart.js
@@ -53,7 +53,7 @@ function updateRadarChart(selectedDataset, selectedYears) {
         
         // Define the angles for each data point
         var radialScale = d3.scaleLinear()
-            .domain([min(0, minTemperature), maxTemperature])
+            .domain([d3.min([0, minTemperature]), maxTemperature])
             .range([0, 200]);
 
         var ticks = [minTemperature, 0, maxTemperature];    
