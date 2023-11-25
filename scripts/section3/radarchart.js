6cb74209bae0578718391c18fec5368b90a53f34
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-25 19:05:20 +0100

    Update radarchart.js

--- scripts/section3/radarchart.js
+++ scripts/section3/radarchart.js
@@ -148,7 +148,28 @@ function updateRadarChart(selectedDataset, selectedYears) {
                     .attr("fill", "none")
                     .attr("stroke-opacity", 1)
                     //.attr("opacity", 0.1)
-            ); 
+            );
+
+            // Calculate coordinates for December and January for each year
+            function getPathCoordinatesDecJan(data_point) {
+                var dec = angleToCoordinate((3 * Math.PI / 2), data_point['Dec']);
+                var jan = angleToCoordinate((Math.PI / 2), data_point['Jan']);
+                return [dec, jan];
+            }
+            
+            // Draw the line connecting December and January for each year
+            svg.selectAll(".dec-jan-line")
+                .data(data)
+                .join(
+                    enter => enter.append("path")
+                        .datum(d => getPathCoordinatesDecJan(d))
+                        .attr("d", line)
+                        .attr("stroke-width", 3)
+                        .attr("stroke", (_, i) => colors[i])
+                        .attr("fill", "none")
+                        .attr("stroke-opacity", 1)
+                        //.attr("opacity", 0.1)
+                );
     });
 }
 
