02a7fa20d9f652a8b16f4504338480069b76d797
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-11-26 13:40:23 +0100

    Update ridgeline.js

--- scripts/section3/ridgeline.js
+++ scripts/section3/ridgeline.js
@@ -90,10 +90,11 @@ function updateRidgeLine(selectedDataset_1, selectedDataset_2, selectedYears) {
 
             var filteredDataMax = months.map( function(month) { return [month, +yearDataMax[0][month]];
                                                               }).filter( function(d) { return !isNaN(d[1]); });
-            
+            console.log(filteredDataMax);
             var filteredDataMin = months.map( function(month) { return [month, +yearDataMin[0][month]];
                                                               }).filter( function(d) { return !isNaN(d[1]); });                     
-
+            console.log(filteredDataMin);
+            
             // Compute kernel density estimation for each column:
             var kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(10)); // increase this 10 for more accurate density.
             density = kde( filteredDataMax.map(function(d){  return d[selectedYear]; }) );
