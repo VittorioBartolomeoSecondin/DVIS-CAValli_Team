fb7aea0fd59c3dde4b76ad1f70dc24395f2aeded
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-11-26 14:06:34 +0100

    Update ridgeline.js

--- scripts/section3/ridgeline.js
+++ scripts/section3/ridgeline.js
@@ -88,16 +88,16 @@ function updateRidgeLine(selectedDataset_1, selectedDataset_2, selectedYears) {
             yearDataMax = dataMax.filter(function (d) { return +d.year === +selectedYear; });
             yearDataMin = dataMin.filter(function (d) { return +d.year === +selectedYear; });  
 
-            var filteredDataMax = months.map( function(month) { return [month, +yearDataMax[0][month]];
+            var filteredDataMax = months.map( function(month) { return [selectedYear, +yearDataMax[0][month]];
                                                               }).filter( function(d) { return !isNaN(d[1]); });
-            console.log(filteredDataMax.slice(1));
-            var filteredDataMin = months.map( function(month) { return [month, +yearDataMin[0][month]];
+            console.log(filteredDataMax);
+            var filteredDataMin = months.map( function(month) { return [selectedYear, +yearDataMin[0][month]];
                                                               }).filter( function(d) { return !isNaN(d[1]); });                     
-            console.log(filteredDataMin.slice(1));
+            console.log(filteredDataMin);
             
             // Compute kernel density estimation for each column:
             var kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(10)); // increase this 10 for more accurate density.
-            densityMax = kde(filteredDataMax.slice(1));
+            densityMax = kde(filteredDataMax);
             console.log(densityMax);
             allDensity.push({key: selectedYear, density: densityMax});
         });
