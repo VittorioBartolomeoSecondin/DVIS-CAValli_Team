08c5d63614395d276a6da0fe065a3e20bad2383a
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-11-30 16:30:16 +0100

    Update radarchart.js

--- scripts/section3/radarchart.js
+++ scripts/section3/radarchart.js
@@ -1,4 +1,4 @@
-function updateRadarChart(selectedDataset, selectedYears) {
+function updateRadarChart(selectedDataset_1, selectedDataset_2, selectedDataset_3, selectedYears) {
    
     // Append the svg object to the body of the page
     var svg = d3.select("#radarchart_1").append("svg")
@@ -10,21 +10,25 @@ function updateRadarChart(selectedDataset, selectedYears) {
 
     // Read the data
     Promise.all([
-        d3.csv(selectedDataset)
+        d3.csv(selectedDataset_1),
+        d3.csv(selectedDataset_2),
+        d3.csv(selectedDataset_3)
     ]).then(function (datasets) {
     
         var dataAvg = datasets[0];
+        var dataMax = datasets[1];
+        var dataMin = datasets[2];
 
         var allMonths = Object.keys(dataAvg[0]).slice(2);
         var months = allMonths.slice(0, allMonths.length / 2);
         
-        var minTemperature = d3.min(dataAvg, function (d) {
+        var minTemperature = d3.min(dataMin, function (d) {
             return d3.min(months, function (month) {
                 return +d[month];
             });
         });
         
-        var maxTemperature = d3.max(dataAvg, function (d) {
+        var maxTemperature = d3.max(dataMax, function (d) {
             return d3.max(months, function (month) {
                 return +d[month];
             });
@@ -116,7 +120,6 @@ function updateRadarChart(selectedDataset, selectedYears) {
         var line = d3.line()
             .x(d => d.x)
             .y(d => d.y);
-        //var colors = ["darkorange", "gray", "navy", "red", "yellow", "purple", "darkgreen", "lightgreen", "lightblue", "pink"];
         
        function getPathCoordinates(data_point){
             var coordinates = [];
@@ -140,8 +143,7 @@ function updateRadarChart(selectedDataset, selectedYears) {
             yearDataAvg = dataAvg.filter(function (d) { return +d.year === +selectedYear; }); 
             var point = {}
             months.forEach(m => point[m] = yearDataAvg[0][m]);
-            data.push(point);
-        
+            data.push(point);      
         
             // Draw paths and circles with the same color for each data point
             svg.selectAll("g")
@@ -210,7 +212,6 @@ function updateRadarChart(selectedDataset, selectedYears) {
                 .style("font-size", "12px");
         });
      });
-
 }
 
 function handleMouseOver(event, d) {
@@ -239,11 +240,13 @@ function handleMouseOut() {
 }
 
 // Initial chart creation with the default dataset
-updateRadarChart("data/section3/AVG/AlabamaAVG.csv", ['2000']);
+updateRadarChart("data/section3/AVG/AlabamaAVG.csv", "data/section3/MAX/AlabamaMAX.csv", "data/section3/MIN/AlabamaMIN.csv", ['2000']);
 
 // Listen for changes in the dropdown selection
 document.getElementById("dataset-dropdown").addEventListener("change", function () {
-  const selectedDataset = "data/section3/AVG/" + this.value + "AVG.csv";
+  const selectedDataset_1 = "data/section3/AVG/" + this.value + "AVG.csv";
+  const selectedDataset_2 = "data/section3/MAX/" + this.value + "MAX.csv";
+  const selectedDataset_3 = "data/section3/MIN/" + this.value + "MIN.csv";
 
   // Select all checked checkboxes
   const checkedCheckboxes = document.querySelectorAll("#year-checkbox-form input:checked");
@@ -252,7 +255,7 @@ document.getElementById("dataset-dropdown").addEventListener("change", function 
   const selectedYears = Array.from(checkedCheckboxes).map(checkbox => checkbox.value);
 
   d3.select("#radarchart_svg").remove();
-  updateRadarChart(selectedDataset, selectedYears);
+  updateRadarChart(selectedDataset_1, selectedDataset_2, selectedDataset_3, selectedYears);
 });
 
 // Add an event listener for changes in the year dropdown
@@ -265,8 +268,10 @@ document.getElementById("year-checkbox-form").addEventListener("change", functio
     // Extract values of checked checkboxes
     const selectedYears = Array.from(checkedCheckboxes).map(checkbox => checkbox.value);
     
-    const selectedDataset = "data/section3/AVG/" + selectedValue + "AVG.csv";
+    const selectedDataset_1 = "data/section3/AVG/" + selectedValue + "AVG.csv";
+    const selectedDataset_2 = "data/section3/MAX/" + selectedValue + "MAX.csv";
+    const selectedDataset_3 = "data/section3/MIN/" + selectedValue + "MIN.csv";
 
     d3.select("#radarchart_svg").remove();
-    updateRadarChart(selectedDataset, selectedYears);
+    updateRadarChart(selectedDataset_1, selectedDataset_2, selectedDataset_3, selectedYears);
 });
