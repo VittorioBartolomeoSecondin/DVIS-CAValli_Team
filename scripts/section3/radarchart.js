bbddbf841f4cebe733eeb9448376e2919dfd897a
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-30 18:09:07 +0100

    Update radarchart.js

--- scripts/section3/radarchart.js
+++ scripts/section3/radarchart.js
@@ -1,4 +1,4 @@
-var margin2 = { top: 60, right: 70, bottom: 70, left: 100 },
+var margin2 = { top: 60, right: 70, bottom: 70, left: 90 },
     width2 = 500 - margin2.left - margin2.right,
     height2 = 700 - margin2.top - margin2.bottom;
 
