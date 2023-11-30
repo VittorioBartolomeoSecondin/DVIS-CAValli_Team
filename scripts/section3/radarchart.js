6255e0b6aadc712c11aaf20b7faf0f8f8e52b70a
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-11-30 17:48:35 +0100

    Update radarchart.js

--- scripts/section3/radarchart.js
+++ scripts/section3/radarchart.js
@@ -1,5 +1,5 @@
 var margin2 = { top: 60, right: 20, bottom: 70, left: 20 },
-    width2 = 800 - margin2.left - margin2.right,
+    width2 = 700 - margin2.left - margin2.right,
     height2 = 700 - margin2.top - margin2.bottom;
 
 function updateRadarChart(selectedDataset_1, selectedDataset_2, selectedDataset_3, selectedYears) {
