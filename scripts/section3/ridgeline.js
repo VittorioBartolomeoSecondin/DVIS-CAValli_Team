4c64dfa2249c95667dd97c2ef308a4329ac11ea1
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2023-11-30 18:17:55 +0100

    Update ridgeline.js

--- scripts/section3/ridgeline.js
+++ scripts/section3/ridgeline.js
@@ -126,7 +126,7 @@ function updateRidgeLine(selectedDataset_1, selectedDataset_2, selectedYears) {
             .datum(function(d) { return d.density; })
             .attr("fill", "#69b3a2")
             .attr("stroke", "#000")
-            .attr("stroke-width", 1);
+            .attr("stroke-width", 1)
             .attr("d", d3.line()
               .curve(d3.curveBasis)
               .x(function(d) { return x(d[0]); })
