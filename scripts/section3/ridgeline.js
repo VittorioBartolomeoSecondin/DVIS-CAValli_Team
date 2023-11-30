2c4ba49ea19cd2f1fc4bbc3106f0d620498396be
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2023-11-30 17:31:37 +0100

    Update ridgeline.js

--- scripts/section3/ridgeline.js
+++ scripts/section3/ridgeline.js
@@ -128,13 +128,13 @@ function updateRidgeLine(selectedDataset_1, selectedDataset_2, selectedYears) {
             })
             .datum(function(d) { return d.density; })
             .attr("fill", "#69b3a2")
-            .attr("stroke", "#000")
-            .attr("stroke-width", 1);
-            /*.attr("d", d3.line()
+            //.attr("stroke", "#000")
+            //.attr("stroke-width", 1);
+            .attr("d", d3.line()
               .curve(d3.curveBasis)
               .x(function(d) { return x(d[0]); })
               .y(function(d) { return y(d[1]); })
-            );*/
+            );
     });
 }
 
