80459b11a554af71792064b68c5bdce1c3abfaf8
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-12-02 18:20:25 +0100

    Update ridgeline.js

--- scripts/section3/ridgeline.js
+++ scripts/section3/ridgeline.js
@@ -36,7 +36,6 @@ function updateRidgeLine(selectedDataset_1, selectedDataset_2, selectedYears) {
         var stateName = selectState.options[selectState.selectedIndex].innerHTML;
 
         var thresholds = d3.ticks(...d3.nice(...[minTemperature, maxTemperature], 2), 12);
-        var thresholdsMinMax = thresholds.push([minTemperature, maxTemperature]);
         
         // Append a title to the SVG
         svg.append("text")
@@ -57,7 +56,7 @@ function updateRidgeLine(selectedDataset_1, selectedDataset_2, selectedYears) {
             .attr("transform", "translate(0," + height + ")")
             .attr("stroke", "gray")
             .attr("stroke-opacity", 0.3)
-            .call(d3.axisBottom(x).tickValues(thresholdsMinMax).tickSize(-height))
+            .call(d3.axisBottom(x).tickValues(thresholds).tickSize(-height))
             .select(".domain").remove();
         
         // Add X axis label:
