86d889a583b40f53557f79fc63c1d664f4c83c40
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-02 21:32:34 +0100

    Update ridgeline.js

--- scripts/section3/ridgeline.js
+++ scripts/section3/ridgeline.js
@@ -57,9 +57,9 @@ function updateRidgeLine(selectedDataset_1, selectedDataset_2, selectedYears) {
             .attr("stroke", "green")
             .attr("stroke-opacity", 1)
             .call(d3.axisBottom(x).tickValues([minTemperature, maxTemperature]).tickSize(-height))
+            .select(".domain").remove()
             .selectAll(".tick line") // Selecting all tick lines
-            .attr("stroke", "green") // Changing the tick color to green
-            .select(".domain").remove();
+            .attr("stroke", "green"); // Changing the tick color to green
         
         svg.append("g")
             .attr("class", "xAxis secondary")
