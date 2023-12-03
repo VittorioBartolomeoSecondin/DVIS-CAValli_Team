fc0b9f309fa4cb3b77ecbbebd41b5e495efbaeae
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-03 13:29:33 +0100

    Update radarchart.js

--- scripts/section3/radarchart.js
+++ scripts/section3/radarchart.js
@@ -169,7 +169,7 @@ function updateRadarChart(selectedDataset_1, selectedDataset_2, selectedDataset_
                     const maxLines = d3.selectAll(`.line-max-${year}`);
                     const minLines = d3.selectAll(`.line-min-${year}`);
                     const avgLines = d3.selectAll(`.line-avg-${year}`);
-                    const legendText = d3.select(`.legend-text-${year}`);
+                    const legendText = d3.select(`.legend-text-${year}`);console.log(legendText);
             
                     if (clickedYear !== previousClickedYear) {
                         circles.style("display", displayStyle);
