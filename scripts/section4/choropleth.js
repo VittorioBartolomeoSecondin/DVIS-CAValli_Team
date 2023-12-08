f658f391ff6cc1a91712eeafadd2bab40312ea62
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2023-12-08 16:54:26 +0100

    Update choropleth.js

--- scripts/section4/choropleth.js
+++ scripts/section4/choropleth.js
@@ -45,10 +45,8 @@ const svg = d3.select("svg"),
     height = +svg.attr("height");
 
 // Map and projection
-const projection = d3.geoMercator()
-    .center([2, 47])                // GPS of location to zoom on
-    .scale(980)                       // This is like the zoom
-    .translate([ width/2, height/2 ])
+const projection = d3.geoAlbersUsa()
+              .scale(1280)
 
 // Load external data and boot
 d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then( function(data){
