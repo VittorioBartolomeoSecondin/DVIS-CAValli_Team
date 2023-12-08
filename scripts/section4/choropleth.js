541ec151848ee4479a435abaa50349e832f6b05d
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-08 16:47:26 +0100

    Update choropleth.js

--- scripts/section4/choropleth.js
+++ scripts/section4/choropleth.js
@@ -1,3 +1,4 @@
+/*
 // initial setup
 const svg = d3.select("svg"),
 	path = d3.geoPath(),
@@ -36,4 +37,33 @@ d3.json(usamap).then(function (us) {
             return d.properties.name + ': ' + dataByFips[d.properties.fips];
           });
 	});
-});
+});*/
+
+// The svg
+const svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+// Map and projection
+const projection = d3.geoMercator()
+    .center([2, 47])                // GPS of location to zoom on
+    .scale(980)                       // This is like the zoom
+    .translate([ width/2, height/2 ])
+
+// Load external data and boot
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then( function(data){
+
+    // Filter data
+    data.features = data.features.filter(d => {console.log(d.properties.name); return d.properties.name=="USA"})
+
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(data.features)
+        .join("path")
+          .attr("fill", "grey")
+          .attr("d", d3.geoPath()
+              .projection(projection)
+          )
+        .style("stroke", "none")
+})
