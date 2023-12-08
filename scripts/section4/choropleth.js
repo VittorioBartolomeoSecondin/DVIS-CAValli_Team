0d9b9025f83b7b32e7758c8cf04d12486dee327c
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-08 23:36:50 +0100

    Update choropleth.js

--- scripts/section4/choropleth.js
+++ scripts/section4/choropleth.js
@@ -35,7 +35,7 @@ fetch("data/section4/choropleth.json")
 			      .attr("width", 1100)
 			      .attr("height", 80)
 			     .append("g")
-			      .attr("transform", `translate(100, -10)`);
+			      .attr("transform", `translate(100, -100)`);
 
 	var colorscale = colours.reverse();
 
