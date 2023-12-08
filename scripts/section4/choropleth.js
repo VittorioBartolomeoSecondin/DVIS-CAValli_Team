d1368e365aa6dc9db339ae0d36d73dcbfdbce297
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-08 17:27:35 +0100

    Update choropleth.js

--- scripts/section4/choropleth.js
+++ scripts/section4/choropleth.js
@@ -40,7 +40,7 @@ d3.json(usamap).then(function (us) {
 });*/
 
 //import * as topojson from "topojson";
-
+/*
 // set the dimensions and margins of the graph
 var margin = { top: 60, right: 70, bottom: 70, left: 100 },
     width = 1000 - margin.left - margin.right,
@@ -69,4 +69,38 @@ g.selectAll(".states")
  .attr("class", "states")
  .attr("d", path);
 
-d3.select(window).on("resize", this.updateSizes.bind(this));
+d3.select(window).on("resize", this.updateSizes.bind(this));*/
+
+// set the dimensions and margins of the graph
+var margin = { top: 60, right: 70, bottom: 70, left: 100 },
+    width = 1000 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
+
+let projection = d3.geoAlbersUsa()
+                   .scale(width)
+                   .translate([width / 2, height / 2]);
+
+let path = d3.geoPath().projection(projection);
+
+let svg = d3.select("#map")
+	    .append("svg")
+	    .attr("width", width)
+	    .attr("height", height)
+	    .attr("preserveAspectRatio", "xMinYMin meet")
+	    .attr("viewBox", `0 0 ${width} ${height}`);
+
+let g = svg.append("g");
+
+// Fetching the JSON file using fetch
+fetch("./us-states.json")
+    .then(response => response.json())
+    .then(data => {
+        g.selectAll(".states")
+            .data(topojson.feature(data, data.objects.states).features)
+            .enter().append("path")
+            .attr("class", "states")
+            .attr("d", path);
+    })
+    .catch(error => {
+        console.error("Error fetching the data:", error);
+    });
