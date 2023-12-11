cab1f8b0a1d1ba968f0b9b41bf3861330f7ca592
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-11 22:38:29 +0100

    Update choropleth.js

--- scripts/section4/choropleth.js
+++ scripts/section4/choropleth.js
@@ -205,4 +205,4 @@ legend_entry.append("text")
 		return d[0] / 1000 + " k - " + d[1] / 1000 + " k";
 	});
 
-legend.append("text").attr("x", 15).attr("y", 280).text("Tree abundance");
+legend.append("text").attr("x", 15).attr("y", 140).text("Tree abundance");
