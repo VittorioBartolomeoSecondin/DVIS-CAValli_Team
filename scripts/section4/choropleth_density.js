fa94ced3642e7f7c78d501b4f6eae3cf8a7461f0
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-13 14:00:38 +0100

    Update choropleth_density.js

--- scripts/section4/choropleth_density.js
+++ scripts/section4/choropleth_density.js
@@ -1,14 +1,10 @@
-let projection = d3.geoAlbersUsa()
-                   .scale(width)
-                   .translate([width / 2, height / 2]);
-
 // add tooltip
-const tooltip = d3.select("body").append("div")
+const tooltip_density = d3.select("body").append("div")
 		  .attr("class", "tooltip")
 		  .style("opacity", 0);
 
-let mouseOver = function(event, d) {
-			d3.selectAll(".Country")
+let mouseOver_density = function(event, d) {
+			d3.selectAll(".Country_density")
 				.transition()
 				.duration(200)
 				.style("opacity", .3)
@@ -20,16 +16,15 @@ let mouseOver = function(event, d) {
 				.style("opacity", 1)
 				.style("stroke", d.properties.density_1000 != 0 ? "green" : "black")
 				.style("stroke-width", "2px");
-  				tooltip.html(d.properties.name + ' &#40;' + d.properties.postal + '&#41;: ' + d.properties.density_1000 
-                    			+ ' trees/1000 square kilometers')
+			tooltip.html(d.properties.name + ' &#40;' + d.properties.postal + '&#41;: ' + d.properties.density_1000 + ' trees/1000 square kilometers')
 				.style("left", (event.pageX + 15) + "px")
 				.style("top", (event.pageY - 28) + "px")
 				.transition().duration(400)
 				.style("opacity", 1);
 		}
 
-let mouseLeave = function() {
-			d3.selectAll(".Country")
+let mouseLeave_density = function() {
+			d3.selectAll(".Country_density")
 				.transition()
 				.duration(200)
 				.style("opacity", 1)
@@ -39,37 +34,22 @@ let mouseLeave = function() {
 				.style("opacity", 0);
 		}
 
-let path = d3.geoPath().projection(projection);
-
 // Define color scale
-const colorScale = d3.scaleThreshold()
+const colorScale_density = d3.scaleThreshold()
 	.domain([100000, 200000, 300000, 500000])
 	.range(d3.schemeGreens[5]);
 
-let svg = d3.select("#choropleth_density")
+let svg_density = d3.select("#choropleth_density")
 	    .append("svg")
 	    .attr("width", width)
 	    .attr("height", height)
 	    .attr("preserveAspectRatio", "xMinYMin meet")
 	    .attr("viewBox", `0 0 ${width} ${height}`);
 
-let world = svg.append("g");
+let world_density = svg.append("g");
 
 // Add the stripe pattern to the SVG
-const defs = svg.append("defs");
-
-defs.append("pattern")
-    .attr("id", "stripe")
-    .attr("patternUnits", "userSpaceOnUse")
-    .attr("width", 8)
-    .attr("height", 8)
-    .attr("patternTransform", "rotate(45)")
-    .append("rect")
-    .attr("width", 4)
-    .attr("height", 8)
-    .attr("transform", "translate(0,0)")
-    .attr("opacity", 0.5)
-    .attr("fill", "grey");
+svg_density.append("defs");
 
 fetch("data/section4/choropleth.json")
     .then(response => response.json())
@@ -84,7 +64,7 @@ fetch("data/section4/choropleth.json")
 	    // add a class, styling and mouseover/mouseleave
 	    .attr("d", path)
 	    .style("stroke", "black")
-	    .attr("class", "Country")
+	    .attr("class", "Country_density")
 	    .attr("id", function(d) { return d.id })
 	    .style("opacity", 1)
 	    .style("stroke-width", "0.75px")
@@ -92,7 +72,7 @@ fetch("data/section4/choropleth.json")
                 // Get data value
                 var value = d.properties.density_1000;
                 //return mapColour(c(value));
-	        return value != 0 ? colorScale(value) : "url(#stripe)";
+	        return value != 0 ? colorScale_density(value) : "url(#stripe)";
             })
 	    .on("mouseover", mouseOver)
 	    .on("mouseleave", mouseLeave);
@@ -102,28 +82,28 @@ fetch("data/section4/choropleth.json")
     });
 
 // Legend
-const x = d3.scaleLinear()
+const x_density = d3.scaleLinear()
 	.domain([2.6, 75.1])
 	.rangeRound([600, 860]);
 
-const legend = svg.append("g")
-	.attr("id", "choropleth_legend")
+const legend_density = svg.append("g")
+	.attr("id", "choropleth_density_legend")
 	.attr("transform", `translate(${legendX}, ${legendY})`);
 
-const legend_entry = legend.selectAll("g.legend")
-	.data(colorScale.range().map(function(d) {
-		d = colorScale.invertExtent(d);
-		if (d[0] == null) d[0] = x.domain()[0];
-		if (d[1] == null) d[1] = x.domain()[1];
+const legend_entry_density = legend_density.selectAll("g.legend_density")
+	.data(colorScale_density.range().map(function(d) {
+		d = colorScale_density.invertExtent(d);
+		if (d[0] == null) d[0] = x_density.domain()[0];
+		if (d[1] == null) d[1] = x_density.domain()[1];
 		return d;
 	}))
 	.enter().append("g")
-	.attr("class", "legend_entry");
+	.attr("class", "legend_entry_density");
 
 const ls_w = 20,
 	ls_h = 20;
 
-legend_entry.append("rect")
+legend_entry_density.append("rect")
 	.attr("x", 20)
 	.attr("y", function(d, i) {
 		return height - (i * ls_h) - 2 * ls_h;
@@ -131,11 +111,11 @@ legend_entry.append("rect")
 	.attr("width", ls_w)
 	.attr("height", ls_h)
 	.style("fill", function(d) {
-		return colorScale(d[0]);
+		return colorScale_density(d[0]);
 	});
 	//.style("opacity", 0.8);
 
-legend_entry.append("text")
+legend_entry_density.append("text")
 	.attr("x", 50)
 	.attr("y", function(d, i) {
 		return height - (i * ls_h) - ls_h - 6;
@@ -146,4 +126,4 @@ legend_entry.append("text")
 		return d[0] / 1000 + " k - " + d[1] / 1000 + " k";
 	});
 
-legend.append("text").attr("x", 15).attr("y", 440).text("Tree abundance");
+legend_density.append("text").attr("x", 15).attr("y", 440).text("Tree abundance");
