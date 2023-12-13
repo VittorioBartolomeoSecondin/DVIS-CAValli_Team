869170f88501c9d618a9a19d99050225cb80c4e2
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-13 13:16:46 +0100

    Update choropleth.js

--- scripts/section4/choropleth.js
+++ scripts/section4/choropleth.js
@@ -37,7 +37,7 @@ let mouseOver = function(event, d) {
 				.style("top", (event.pageY - 28) + "px")
 				.transition().duration(400)
 				.style("opacity", 1)
-				.text(function(d) {
+				.text(function() {
 				     return d.properties.name + " (" + d.properties.postal + "): " + d.properties.abundance + " trees"
 				     + "\n" + d.properties.area + " square kilometers";
     		    		});
