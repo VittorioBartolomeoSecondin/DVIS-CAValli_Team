07eb24e970669811195ca4e1cafe8963b4cad79e
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-13 13:31:48 +0100

    Update choropleth.js

--- scripts/section4/choropleth.js
+++ scripts/section4/choropleth.js
@@ -39,10 +39,6 @@ let mouseOver = function(event, d) {
 				.style("top", (event.pageY - 28) + "px")
 				.transition().duration(400)
 				.style("opacity", 1);
-				/*.text(function() {
-				     return d.properties.name + " (" + d.properties.postal + "): " + d.properties.abundance + " trees"
-				     + "\nArea: " + d.properties.area + " square kilometers";
-    		    		});*/
 		}
 
 let mouseLeave = function() {
@@ -62,12 +58,6 @@ let path = d3.geoPath().projection(projection);
 const colorScale = d3.scaleThreshold()
 	.domain([100000, 200000, 300000, 500000])
 	.range(d3.schemeGreens[5]);
-
-/*var colours = d3.schemeGreens[9].reverse();
-  
-var mapColour = d3.scaleLinear()
-		      .domain(d3.range(0, 1, 1.0 / (colours.length - 1)))
-		      .range(colours);*/
 
 let svg = d3.select("#map")
 	    .append("svg")
@@ -99,63 +89,6 @@ fetch("data/section4/choropleth.json")
     .then(data => {
         const data_features = topojson.feature(data, data.objects.states).features;
 	    
-	/*const legend_svg = svg.append("g")
-			      .attr("id", "choropleth_legend_svg")
-			      .attr("width", legendWidth)
-			      .attr("height", legendHeight)
-			     .append("g")
-			      .attr("transform", `translate(${legendX}, ${legendY})`);
-
-	var min = d3.min(data_features, function(d) { return +d.properties.abundance}); 
-        var max = d3.max(data_features, function(d) { return +d.properties.abundance});
-
-	var c = d3.scaleLinear().domain([max, 0]).range([0,1]);
-	    
-	var color = d3.scaleQuantize()
-		      .domain([max, 0])
-		      .range(colours);
-	  
-	var format = d3.format(".0f")
-	  
-	drawColorScale();
-	  
-	function drawColorScale() {
-	      var palette = legend_svg.append('g')
-		.attr('id', 'palette');
-	
-	      // fill the legend with rectangles (colours)
-	      var swatch = palette.selectAll('rect').data(colours);
-	      swatch.enter().append('rect')
-		.attr('fill', function(d) {
-		  return d;
-		})
-		.attr('y', function(d, i) {
-		  return i * 50;
-		})
-		.attr('x', 50)
-		.attr('height', 50)
-		.attr('width', 20)
-		.style("stroke-width", 1)
-		.style("stroke", "black");
-	
-	      // counts are placed on one side of the legend
-	      var texts = palette.selectAll("foo")
-		.data(color.range())
-		.enter()
-		.append("text")
-		.attr("font-size", "10px")
-		.attr("text-anchor", "middle")
-		.attr("x", 80)
-		.attr('y', function(d, i) {
-		  return 25 + 50 * i;
-		})
-		.attr("dx", "2em")
-		// single value for each rectangle in the legend
-		.text(function(d) {
-		  return `< ${(color.invertExtent(d)[0] / (10 ** 6)).toFixed(2)} m.`;
-		})
-	}*/
-
         world.selectAll(".states")
             .data(data_features)
             .enter().append("path")
