8af975e506bb3199f9faf3eb24497bd3b81a15b4
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-13 17:36:10 +0100

    Update choropleth_density.js

--- scripts/section4/choropleth_density.js
+++ scripts/section4/choropleth_density.js
@@ -161,7 +161,7 @@ const ChoroplethDensity = {
 			});
 		
 		legend.append("text")
-		  .attr("x", 12)
+		  .attr("x", 5)
 		  .attr("y", 425)
 		  .html('Tree density')
 		  .append("tspan") // Append a tspan for the second line
