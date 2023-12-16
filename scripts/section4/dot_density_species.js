cc81947f4be8a9f035805402c47c9101f9dde821
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-16 17:56:27 +0100

    Update dot_density_species.js

--- scripts/section4/dot_density_species.js
+++ scripts/section4/dot_density_species.js
@@ -2,7 +2,7 @@ const DotDensitySpecies = {
   	initialize: function() {
     		// set the dimensions and margins of the graph
     		var margin = { top: 60, right: 70, bottom: 70, left: 100 },
-    		    width = 1420 - margin.left - margin.right,
+    		    width = 1435 - margin.left - margin.right,
     		    height = 700 - margin.top - margin.bottom;
     		
     		let projection = d3.geoAlbersUsa()
