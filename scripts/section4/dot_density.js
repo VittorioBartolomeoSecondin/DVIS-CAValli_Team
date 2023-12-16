0927c2032339cd264781de88ca18a33735b98375
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-16 12:37:52 +0100

    Update dot_density.js

--- scripts/section4/dot_density.js
+++ scripts/section4/dot_density.js
@@ -1,4 +1,4 @@
-const Dot_Density = {
+const DotDensity = {
 	initialize: function() {
 		// set the dimensions and margins of the graph
 		var margin = { top: 60, right: 70, bottom: 70, left: 100 },
@@ -119,4 +119,4 @@ const Dot_Density = {
 	}
 }
 
-Dot_Density.initialize();
+DotDensity.initialize();
