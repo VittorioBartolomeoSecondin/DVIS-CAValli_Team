7064a85617bf8f48cc93ac5ddfe946ae8d433d8c
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-17 11:37:24 +0100

    Update dot_density.js

--- scripts/section4/dot_density.js
+++ scripts/section4/dot_density.js
@@ -40,11 +40,18 @@ const DotDensity = {
 		}
 
 		let mouseOver_states = function(event, d) {
+		     d3.selectAll(".Country")
+			.transition()
+			.duration(200)
+			.style("opacity", .3)
+			.style("stroke", "black")
+			.style("stroke-width", "0.75px");
 		     d3.select(this)
 			.transition()
 			.duration(200)
+			.style("opacity", 1)
+			.style("stroke", d.properties.abundance != 0 ? "green" : "black")
 			.style("stroke-width", "2px");
-			
 		     tooltip.html(d.properties.name + ' &#40;' + d.properties.postal + '&#41;')
 			.style("left", (event.pageX + 15) + "px")
 			.style("top", (event.pageY - 28) + "px")
@@ -53,12 +60,13 @@ const DotDensity = {
 		}
 		
 		let mouseLeave_states = function() {
-		     d3.select(this)
+		     d3.selectAll(".Country")
 			.transition()
 			.duration(200)
-			.style("stroke-width", "0.75px")
-	
-		    tooltip.transition().duration(300)
+			.style("opacity", 1)
+			.style("stroke", "black")
+			.style("stroke-width", "0.75px");
+		     tooltip.transition().duration(300)
 			.style("opacity", 0);
 		}
 		
